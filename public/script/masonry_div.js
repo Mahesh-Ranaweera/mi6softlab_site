ed404a1ef63757f5fd5e9130468803e7e84b81b6
Mahesh Ranaweera <mi6softlab@gmail.com>
2017-03-10 12:11:56 -0500

    Added links

---
+++ public/script/masonry_div.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function () {
+    $('.app_page_wrapper').masonry({
+        itemSelector: '.app_card_cover',
+        columnWidth: '.app_card_cover',
+        percentPosition: true
+    });
+});
