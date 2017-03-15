cb83163f8e60ab359c36038ed57b17c425468880
Mahesh Ranaweera <mahesh.ranaweerakaluarachchige@uoit.net>
2017-03-15 15:44:41 -0400

    Updated masonry

--- public/script/masonry_div.js
+++ public/script/masonry_div.js
@@ -1,8 +1,18 @@
 
+/*
 $(document).ready(function () {
     $('.app_page_wrapper').masonry({
         itemSelector: '.app_card_cover',
         columnWidth: '.app_card_cover',
         percentPosition: true
     });
-});
+}); 
+
+*/
+var $grid = $('.app_page_wrapper').imagesLoaded(function(){
+    $grid.masonry({
+         itemSelector: '.app_card_cover',
+        columnWidth: '.app_card_cover',
+        percentPosition: true
+    })
+})
